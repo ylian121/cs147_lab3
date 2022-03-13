2dedb1ae87ce80bb8d1d7941a74e9eef04a04aa7
Daniel Wong <danx255@gmail.com>
2022-03-13 14:46:46 -0700

    Initial commit

---
+++ kernel.cu
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define TILE_SIZE 16
+
+__global__ void mysgemm(int m, int n, int k, const float *A, const float *B, float* C) {
+
+    /********************************************************************
+     *
+     * Compute C = A x B
+     *   where A is a (m x k) matrix
+     *   where B is a (k x n) matrix
+     *   where C is a (m x n) matrix
+     *
+     * Use shared memory for tiling
+     *
+     ********************************************************************/
+
+    /*************************************************************************/
+    // INSERT KERNEL CODE HERE
+        
+    /*************************************************************************/
+}
+
+void basicSgemm(int m, int n, int k, const float *A, const float *B, float *C)
+{
+    // Initialize thread block and kernel grid dimensions ---------------------
+
+    const unsigned int BLOCK_SIZE = TILE_SIZE;
+	
+    /*************************************************************************/
+    //INSERT CODE HERE
+
+    /*************************************************************************/
+
+    // Invoke CUDA kernel -----------------------------------------------------
+
+    /*************************************************************************/
+    //INSERT CODE HERE
+	
+    /*************************************************************************/
+}
+
+
